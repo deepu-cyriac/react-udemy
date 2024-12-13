6e8f0eaf060751646fde2a2a6b6d94dd72ff1f87
Deepu Cyriac <deepucvk123@gmail.com>
2024-12-13 09:23:45 +0530

    Added new project component

--- project-management-app/src/App.jsx
+++ project-management-app/src/App.jsx
@@ -1,10 +1,12 @@
+import NewProject from "./components/NewProject";
 import ProjectsSidebar from "./components/ProjectsSidebar";
 
 function App() {
   return (
     //tailwind - h-screen - takes all screen height
-    <main className="h-screen my-8">
+    <main className="h-screen my-8 flex gap-8">
       <ProjectsSidebar />
+      <NewProject />
     </main>
   );
 }
