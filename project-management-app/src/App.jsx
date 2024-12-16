68ba4310213cfd56187f90b2a0c2d4af7b5dff13
Deepu Cyriac <deepucvk123@gmail.com>
2024-12-16 09:30:10 +0530

    Added no projects selected component

--- project-management-app/src/App.jsx
+++ project-management-app/src/App.jsx
@@ -1,4 +1,5 @@
 import NewProject from "./components/NewProject";
+import NoProjectSelected from "./components/NoProjectSelected";
 import ProjectsSidebar from "./components/ProjectsSidebar";
 
 function App() {
@@ -6,7 +7,7 @@ function App() {
     //tailwind - h-screen - takes all screen height
     <main className="h-screen my-8 flex gap-8">
       <ProjectsSidebar />
-      <NewProject />
+      <NoProjectSelected />
     </main>
   );
 }
