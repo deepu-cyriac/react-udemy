8e45a60683464f877ff6e2171a896c5866e0f9b7
Deepu Cyriac <deepucvk123@gmail.com>
2024-12-17 08:59:31 +0530

    Added simple modal for form validation error display

--- project-management-app/src/components/NewProject.jsx
+++ project-management-app/src/components/NewProject.jsx
@@ -1,8 +1,11 @@
 import { useRef } from "react";
 
 import Input from "./Input";
+import Modal from "./Modal";
 
 export default function NewProject({ onAdd }) {
+  const modal = useRef();
+
   const title = useRef();
   const description = useRef();
   const dueDate = useRef();
@@ -14,6 +17,15 @@ export default function NewProject({ onAdd }) {
 
     //add validation
 
+    if (
+      enteredTitle.trim() === "" ||
+      enteredDescription.trim() === "" ||
+      enteredDueDate.trim() === ""
+    ) {
+      modal.current.open();
+      return;
+    }
+
     onAdd({
       title: enteredTitle,
       description: enteredDescription,
@@ -22,28 +34,34 @@ export default function NewProject({ onAdd }) {
   }
 
   return (
-    <div className="w-[35rem] mt-16">
-      <menu className="flex items-center justify-end gap-4 my-4">
-        <li>
-          <button className="text-stone-800 hover:text-stone-950">
-            Cancel
-          </button>
-        </li>
-        <li>
-          <button
-            onClick={handleSave}
-            className=" px-6 py-2 rounded-md bg-stone-800 text-stone-50 hover:bg-stone-950"
-          >
-            Save
-          </button>
-        </li>
-      </menu>
+    <>
+      <Modal ref={modal} buttonCaption="Close">
+        <h2>Invalid Input</h2>
+        <p>Oops ... Looks like you forgot to enter a value</p>
+      </Modal>
+      <div className="w-[35rem] mt-16">
+        <menu className="flex items-center justify-end gap-4 my-4">
+          <li>
+            <button className="text-stone-800 hover:text-stone-950">
+              Cancel
+            </button>
+          </li>
+          <li>
+            <button
+              onClick={handleSave}
+              className=" px-6 py-2 rounded-md bg-stone-800 text-stone-50 hover:bg-stone-950"
+            >
+              Save
+            </button>
+          </li>
+        </menu>
 
-      <div>
-        <Input label="Title" ref={title} />
-        <Input label="Description" textarea={true} ref={description} />
-        <Input label="Due Date" ref={dueDate} date={true} />
+        <div>
+          <Input label="Title" ref={title} />
+          <Input label="Description" textarea={true} ref={description} />
+          <Input label="Due Date" ref={dueDate} date={true} />
+        </div>
       </div>
-    </div>
+    </>
   );
 }
