60e494ed5172dd29029e2ff430b9a8698dd8284b
Deepu Cyriac <deepucvk123@gmail.com>
2025-01-10 22:25:04 +0530

    Added basic banner

--- farmup/src/components/Banner/Banner.tsx
+++ farmup/src/components/Banner/Banner.tsx
@@ -1,9 +1,22 @@
+import { useState, useEffect } from "react";
+
 import "./Banner.css";
 
 const Banner: React.FC = () => {
+  const images: Number[] = [0, 1, 2];
+  const [index, setIndex] = useState(0);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setIndex((prevIndex) => (prevIndex + 1) % images.length);
+    }, 2000);
+
+    return () => clearInterval(interval);
+  }, []);
+
   return (
     <div>
-      <p>This is the banner!</p>
+      <p>This is the banner! {index}</p>
     </div>
   );
 };
