541b9bb48181f1075ab582f9c5d4074c3211544a
Deepu Cyriac <deepucvk123@gmail.com>
2025-01-11 11:49:17 +0530

    Added dummy posts

--- farmup/src/pages/Home/Home.tsx
+++ farmup/src/pages/Home/Home.tsx
@@ -2,6 +2,8 @@ import Header from "../../components/Header/Header";
 import Footer from "../../components/Footer/Footer";
 import Banner from "../../components/Banner/Banner";
 
+import { Card, Button } from "react-bootstrap";
+
 import "./Home.css";
 
 const Home: React.FC = () => {
@@ -12,9 +14,67 @@ const Home: React.FC = () => {
           <Header />
         </header>
 
-        <main>
-          <div className="carousel-overlay"></div>
-          <Banner />
+        <div className="carousel-overlay"></div>
+        <Banner />
+        <main className="m-3">
+          <div className="d-flex justify-content-around flex-wrap">
+            <Card className="m-1">
+              <Card.Img
+                variant="top"
+                src="https://media.istockphoto.com/id/543212762/photo/tractor-cultivating-field-at-spring.jpg?s=612x612&w=0&k=20&c=uJDy7MECNZeHDKfUrLNeQuT7A1IqQe89lmLREhjIJYU="
+              />
+              <Card.Body>
+                <Card.Title>Card Title</Card.Title>
+                <Card.Text>
+                  Some quick example text to build on the card title and make up
+                  the bulk of the card's content.
+                </Card.Text>
+                <Button variant="primary">Go somewhere</Button>
+              </Card.Body>
+            </Card>
+            <Card className="m-1">
+              <Card.Img
+                variant="top"
+                src="https://media.istockphoto.com/id/543212762/photo/tractor-cultivating-field-at-spring.jpg?s=612x612&w=0&k=20&c=uJDy7MECNZeHDKfUrLNeQuT7A1IqQe89lmLREhjIJYU="
+              />
+              <Card.Body>
+                <Card.Title>Card Title</Card.Title>
+                <Card.Text>
+                  Some quick example text to build on the card title and make up
+                  the bulk of the card's content.
+                </Card.Text>
+                <Button variant="primary">Go somewhere</Button>
+              </Card.Body>
+            </Card>
+            <Card className="m-1">
+              <Card.Img
+                variant="top"
+                src="https://media.istockphoto.com/id/543212762/photo/tractor-cultivating-field-at-spring.jpg?s=612x612&w=0&k=20&c=uJDy7MECNZeHDKfUrLNeQuT7A1IqQe89lmLREhjIJYU="
+              />
+              <Card.Body>
+                <Card.Title>Card Title</Card.Title>
+                <Card.Text>
+                  Some quick example text to build on the card title and make up
+                  the bulk of the card's content.
+                </Card.Text>
+                <Button variant="primary">Go somewhere</Button>
+              </Card.Body>
+            </Card>
+            <Card className="m-1">
+              <Card.Img
+                variant="top"
+                src="https://media.istockphoto.com/id/543212762/photo/tractor-cultivating-field-at-spring.jpg?s=612x612&w=0&k=20&c=uJDy7MECNZeHDKfUrLNeQuT7A1IqQe89lmLREhjIJYU="
+              />
+              <Card.Body>
+                <Card.Title>Card Title</Card.Title>
+                <Card.Text>
+                  Some quick example text to build on the card title and make up
+                  the bulk of the card's content.
+                </Card.Text>
+                <Button variant="primary">Go somewhere</Button>
+              </Card.Body>
+            </Card>
+          </div>
         </main>
       </div>
 
